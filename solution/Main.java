67649814445806cbf1daac9e4a970f0c74f72c91
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-01-10 08:02:41 +0900

    Add files via upload

---
+++ solution/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            bw.write(a+"\n");
+        }
+        bw.flush();
+    }
+}

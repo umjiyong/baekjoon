a53110c636b3f2573b6f0e03bdcc0cca7e77e75a
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-01-20 20:50:43 +0900

    Create [B2] BJ_15596.java

---
+++ solution/[B2] BJ_15596.java
@@ -0,0 +1,11 @@
+public class Test {
+    long sum(int[] a) {
+        long num = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            num += a[i];
+        }
+        return num;
+    };
+    
+}

fa3558341a47c19cbc7af6b98d5a9cab8ab0eb09
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-02-07 03:09:19 +0900

    Add files via upload

---
+++ solution/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//        Scanner sc = new Scanner(System.in);
+
+        StringTokenizer st;
+//        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<String> set = new HashSet<>();
+
+
+        for (int i = 0; i < N; i++){
+           set.add(br.readLine());
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < M; i++){
+            if (set.contains(br.readLine())) answer++;
+        }
+
+
+       System.out.println(answer);
+
+    }
+}

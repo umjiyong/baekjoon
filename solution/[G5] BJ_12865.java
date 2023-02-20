c11e1b98a2646944a1443a3e00b27a8b4988684f
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-02-21 01:16:00 +0900

    G5 백준 12865 (평범한 배낭)

--- solution/[G5] BJ_12865.java
+++ solution/[G5] BJ_12865.java
@@ -13,30 +13,28 @@ public class Main {
 //        Scanner sc = new Scanner(System.in);
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken()); // 물건의 개수
+        K = Integer.parseInt(st.nextToken()); // 가방의 제한 무게
 
         int [][] backpack = new int [N+1][K+1];
 
         for (int i = 1; i < N+1; i++) {
             st = new StringTokenizer(br.readLine());
-            W = Integer.parseInt(st.nextToken());
-            V = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken()); // 물건의 무게
+            V = Integer.parseInt(st.nextToken()); // 물건의 가치
 
-            for (int j = 0; j < W; j++){ // 가방에 들어 갈 수 없는 물건 (물건 하나의 무게가 제한 사항 보다 높기 때문에)
-                if (j > K) { // INDEX
+            for (int j = 0; j < W; j++){ // 가방에 물건이 들어갈 수 없는 경우 (물건 하나의 무게가 제한 사항 보다 높기 때문에)
+                if (j > K){
                     break;
                 }
-                    backpack[i][j] = backpack[i-1][j];
+                backpack[i][j] = backpack[i-1][j];
             }
 
-            for (int j = W; j < K+1; j++){ // 현재 물건을 넣기 전과 비교
-                backpack [i][j] = Math.max(backpack[i-1][j-W]+V,backpack[i-1][j]);
+            for (int j = W; j < K+1; j++){// 가방에 물건을 넣었을 때와 넣지 않았을 때를 비교하여 가치의 최댓값을 넣어줌
+                backpack[i][j] = Math.max(backpack[i-1][j-W]+V,backpack[i-1][j]);
             }
         }
-
         bw.write(Integer.toString(backpack[N][K]));
-
         bw.flush();
     }
 }

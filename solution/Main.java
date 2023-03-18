2f530cd89de7204dabfce56c7cbfd99734f65a0b
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-03-18 16:56:05 +0900

    Add files via upload

---
+++ solution/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(N+M));
+        bw.flush();
+
+
+    }
+
+
+}

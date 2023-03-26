729bc1aeb00c01bb3b18d49a9b6242de58748392
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-03-26 18:37:37 +0900

    B5 백준 24262 (알고리즘 수업 - 알고리즘의 수행 시간 1)

--- solution/[B5] BJ_24262.java
+++ solution/[B5] BJ_24262.java
@@ -1,30 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-        int N = Integer.parseInt(br.readLine());
-
-        int max = -1001;
-
-        int sum = 0;
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++){
-            int temp = Integer.parseInt(st.nextToken());
-
-            sum += temp;
-
-            max = Math.max(max,sum);
-
-            if (sum < 0) sum = 0;
-        }
-        System.out.println(max);
-
+        System.out.println(1);
+        System.out.println(0);
     }
 }

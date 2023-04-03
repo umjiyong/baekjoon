0b548d565dac1d810d497bab058e5999c0a122e3
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-04-03 20:10:23 +0900

    Add files via upload

---
+++ solution/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+
+        int ans[] = new int[N];
+
+        ans[0] = 1;
+        ans[1] = 3;
+
+        for (int i = 2; i < N; i++) {
+            ans[i] = ans[i - 1] + 2 * ans[i - 2];
+
+            if (ans[i] > 10007) {
+                ans[i] %= 10007;
+            }
+        }
+
+        System.out.println(ans[N - 1]);
+    }
+}
+

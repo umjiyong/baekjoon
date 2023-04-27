fc5a7d469d2077509cbf6023e1c66feedd83b23e
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-04-27 21:45:29 +0900

    Add files via upload

---
+++ solution/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        System.out.printf("%.0f",Math.pow(Math.pow(2,N)+1,2));
+    }
+}

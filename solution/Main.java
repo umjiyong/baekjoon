34ecd12883056a8a0a8121474610ede2e8c578d8
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-05-08 21:25:09 +0900

    Add files via upload

---
+++ solution/Main.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int n, m;
+    private static int[] map;
+    private static int[] result;
+    private static boolean[] visit;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        map = new int[n];
+        result = new int[m];
+        visit = new boolean[n+1];
+
+        for (int i=0; i<n; i++) {
+            map[i] = sc.nextInt();
+        }
+
+        Arrays.sort(map);
+
+        StringBuilder sb = new StringBuilder();
+
+        cycle(0, sb);
+
+        System.out.print(sb);
+
+    }
+
+    private static void cycle(int start, StringBuilder sb) {
+
+        if (start == m) {
+
+            for (int i=0; i<m; i++) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+
+        } else {
+
+            ArrayList<Integer> list = new ArrayList<Integer>();
+            for (int i=0; i<map.length; i++) {
+
+                if (!visit[i]) {
+
+                    //2번째 원소중 이전에 나온것 또 나오면 안됨
+                    int cnt = 0;
+                    for (int j=0; j<list.size(); j++) {
+                        if (map[i] == list.get(j)) {
+                            cnt++;
+                        }
+                    }
+
+                    if (cnt == 0) {
+                        //중복 x
+                        visit[i] = true;
+                        result[start] = map[i];
+                        list.add(map[i]);
+                        cycle(start+1, sb);
+                        visit[i] = false;
+
+                    } else {
+                        //중복 o
+                        continue;
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+}

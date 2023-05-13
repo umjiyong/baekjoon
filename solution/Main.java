42215c4ea65d7e7a6596aba70ddb5c03181cdeab
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-05-13 19:49:45 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -1,77 +1,41 @@
 import java.io.*;
-import java.util.*;
+
 
 public class Main {
-    static int N, M, X;
-    static ArrayList<info>[] arr;
+	static int n;
+	static StringBuilder answer;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        ArrayList<info>[] arr_reverse = new ArrayList[N+1];
-        arr = new ArrayList[N+1];
-        for(int i=1; i<=N; i++) {
-            arr[i] = new ArrayList<>();
-            arr_reverse[i] = new ArrayList<>();
-        }
-        for(int i=1; i<=M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x =	 Integer.parseInt(st.nextToken());
-            int y =	 Integer.parseInt(st.nextToken());
-            int m =  Integer.parseInt(st.nextToken());
-            arr[x].add(new info(y, m));
-            arr_reverse[y].add(new info(x, m));
-        }
-        int[] dist = new int[N+1];
-        int[] dist2 = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        Arrays.fill(dist2, Integer.MAX_VALUE);
-        dist[X] = 0; dist2[X] = 0;
-        boolean[] visited = new boolean[N+1];
-        PriorityQueue<info> q = new PriorityQueue<>();
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr[now.v]) {
-                if(dist[t.v] > t.weight + dist[now.v]) {
-                    dist[t.v] = t.weight + dist[now.v];
-                    q.add(new info(t.v, dist[t.v]));
-                }
-            }
-        }
-        Arrays.fill(visited, false);
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr_reverse[now.v]) {
-                if(dist2[t.v] > t.weight + dist2[now.v]) {
-                    dist2[t.v] = t.weight + dist2[now.v];
-                    q.add(new info(t.v, dist2[t.v]));
-                }
-            }
-        }
-        int max = 0;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, dist[i] + dist2[i]);
-        }
-        System.out.println(max);
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-    static class info implements Comparable<info>{
-        int v, weight;
-        info(int v, int weight){
-            this.v = v;
-            this.weight = weight;
-        }
-        @Override
-        public int compareTo(info o) {
-            return weight - o.weight;
-        }
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		while ((str = br.readLine()) != null) {
+			n = Integer.parseInt(str);
+			answer =new StringBuilder();
+			int len = (int) Math.pow(3, n);
+            //길이가 3^n 이고 '-'로 이루어진 문자열 미리 만들어 놓고, 재귀함수 시작하기 
+			for (int i = 0; i < len; i++) {
+				answer.append("-");
+			}
+			
+			func(0, len); // 시작 인덱스, 시작 길이
+			System.out.println(answer);
+
+		}
+	}
+
+	public static void func(int start,int size) { //시작 인덱스, 문자열 사이즈
+		if(size==1) { //문자열 사이즈가 1이되면 리턴
+			return;
+		}
+		int newSize=size/3;
+        // 3등분한 문자열의 2번째 부분 공백처리
+		for(int i=start+newSize; i<start+2*newSize; i++) {
+			answer.setCharAt(i, ' ');
+		}
+		
+		func(start, newSize); // 3등분한 문자열에서 첫번째 부분
+		func(start+2*newSize, newSize); // 3등분한 문자열의 세번째 부분 
+	}
+
 }

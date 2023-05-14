92cedc2b00063655fb9b6994a942afddb0c95088
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-05-14 21:04:38 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -1,77 +1,42 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M, X;
-    static ArrayList<info>[] arr;
-
-    public static void main(String[] args) throws IOException {
+ 
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        ArrayList<info>[] arr_reverse = new ArrayList[N+1];
-        arr = new ArrayList[N+1];
-        for(int i=1; i<=N; i++) {
-            arr[i] = new ArrayList<>();
-            arr_reverse[i] = new ArrayList<>();
-        }
-        for(int i=1; i<=M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x =	 Integer.parseInt(st.nextToken());
-            int y =	 Integer.parseInt(st.nextToken());
-            int m =  Integer.parseInt(st.nextToken());
-            arr[x].add(new info(y, m));
-            arr_reverse[y].add(new info(x, m));
-        }
-        int[] dist = new int[N+1];
-        int[] dist2 = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        Arrays.fill(dist2, Integer.MAX_VALUE);
-        dist[X] = 0; dist2[X] = 0;
-        boolean[] visited = new boolean[N+1];
-        PriorityQueue<info> q = new PriorityQueue<>();
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr[now.v]) {
-                if(dist[t.v] > t.weight + dist[now.v]) {
-                    dist[t.v] = t.weight + dist[now.v];
-                    q.add(new info(t.v, dist[t.v]));
-                }
+ 
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+ 
+        int ans = 1;
+        while (B != A) {
+            if (B < A) {
+                ans = -1;
+                break;
             }
-        }
-        Arrays.fill(visited, false);
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr_reverse[now.v]) {
-                if(dist2[t.v] > t.weight + dist2[now.v]) {
-                    dist2[t.v] = t.weight + dist2[now.v];
-                    q.add(new info(t.v, dist2[t.v]));
-                }
+ 
+            String str = String.valueOf(B);
+ 
+            // 맨 끝자리가 1이거나 B가 2로 나누어 떨어지지 않는다면, A로 만들 수 없다.
+            if (str.charAt(str.length() - 1) != '1' && B % 2 != 0) {
+                ans = -1;
+                break;
             }
+ 
+            if (B % 2 == 0) { // B가 2로 나누어 떨어진다면, 2로 나눈다.
+                B >>= 1;
+            } else { // 그렇지 않고, 맨 끝자리가 1이라면 1을 없앤다.
+                str = str.substring(0, str.length() - 1);
+                B = Long.parseLong(str);
+            }
+ 
+            ans++; // 횟수 증가.
         }
-        int max = 0;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, dist[i] + dist2[i]);
-        }
-        System.out.println(max);
-    }
-
-    static class info implements Comparable<info>{
-        int v, weight;
-        info(int v, int weight){
-            this.v = v;
-            this.weight = weight;
-        }
-        @Override
-        public int compareTo(info o) {
-            return weight - o.weight;
-        }
+ 
+        bw.write(ans + "\n");
+        bw.flush();
     }
 }

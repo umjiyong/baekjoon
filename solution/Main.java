8cdaa4e48d5d093e6db7d06719392fb53b00ec06
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-05-16 08:41:32 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -2,76 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, X;
-    static ArrayList<info>[] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        ArrayList<info>[] arr_reverse = new ArrayList[N+1];
-        arr = new ArrayList[N+1];
-        for(int i=1; i<=N; i++) {
-            arr[i] = new ArrayList<>();
-            arr_reverse[i] = new ArrayList<>();
-        }
-        for(int i=1; i<=M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x =	 Integer.parseInt(st.nextToken());
-            int y =	 Integer.parseInt(st.nextToken());
-            int m =  Integer.parseInt(st.nextToken());
-            arr[x].add(new info(y, m));
-            arr_reverse[y].add(new info(x, m));
-        }
-        int[] dist = new int[N+1];
-        int[] dist2 = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        Arrays.fill(dist2, Integer.MAX_VALUE);
-        dist[X] = 0; dist2[X] = 0;
-        boolean[] visited = new boolean[N+1];
-        PriorityQueue<info> q = new PriorityQueue<>();
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr[now.v]) {
-                if(dist[t.v] > t.weight + dist[now.v]) {
-                    dist[t.v] = t.weight + dist[now.v];
-                    q.add(new info(t.v, dist[t.v]));
-                }
-            }
-        }
-        Arrays.fill(visited, false);
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr_reverse[now.v]) {
-                if(dist2[t.v] > t.weight + dist2[now.v]) {
-                    dist2[t.v] = t.weight + dist2[now.v];
-                    q.add(new info(t.v, dist2[t.v]));
-                }
-            }
-        }
-        int max = 0;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, dist[i] + dist2[i]);
-        }
-        System.out.println(max);
-    }
 
-    static class info implements Comparable<info>{
-        int v, weight;
-        info(int v, int weight){
-            this.v = v;
-            this.weight = weight;
-        }
-        @Override
-        public int compareTo(info o) {
-            return weight - o.weight;
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < tc; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            // initialize
+            int[][] stickers = new int[2][n+1];
+            int[][] dp = new int[2][n+1];
+            for(int j=0; j<2; j++){
+                String[] inputs = br.readLine().split(" ");
+                for (int k = 1; k <= n; k++) {
+                    stickers[j][k] = Integer.parseInt(inputs[k-1]);
+                }
+            }
+
+	    // 첫번째 column은 자기 자신이 최대이므로 자기 자신으로 초기화
+            dp[0][1] = stickers[0][1];
+            dp[1][1] = stickers[1][1];
+
+            for (int j = 2; j <= n; j++) {
+                dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + stickers[0][j];
+                dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + stickers[1][j];
+            }
+
+            System.out.println(Math.max(dp[0][n], dp[1][n]));
         }
     }
 }

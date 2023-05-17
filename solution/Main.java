c84ad11e20c679a2cc83f3fd163a61ed675e7d9c
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-05-17 22:23:15 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -1,77 +1,60 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M, X;
-    static ArrayList<info>[] arr;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        ArrayList<info>[] arr_reverse = new ArrayList[N+1];
-        arr = new ArrayList[N+1];
-        for(int i=1; i<=N; i++) {
-            arr[i] = new ArrayList<>();
-            arr_reverse[i] = new ArrayList<>();
+    static int n,m;
+    static int card[];
+    static int store[];
+    static StringBuilder sb = new StringBuilder();
+    static HashSet<String> set = new HashSet<>();
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        n = scan.nextInt();
+        m = scan.nextInt();
+
+        card = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            card[i] = scan.nextInt();
         }
-        for(int i=1; i<=M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x =	 Integer.parseInt(st.nextToken());
-            int y =	 Integer.parseInt(st.nextToken());
-            int m =  Integer.parseInt(st.nextToken());
-            arr[x].add(new info(y, m));
-            arr_reverse[y].add(new info(x, m));
-        }
-        int[] dist = new int[N+1];
-        int[] dist2 = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        Arrays.fill(dist2, Integer.MAX_VALUE);
-        dist[X] = 0; dist2[X] = 0;
-        boolean[] visited = new boolean[N+1];
-        PriorityQueue<info> q = new PriorityQueue<>();
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr[now.v]) {
-                if(dist[t.v] > t.weight + dist[now.v]) {
-                    dist[t.v] = t.weight + dist[now.v];
-                    q.add(new info(t.v, dist[t.v]));
-                }
-            }
-        }
-        Arrays.fill(visited, false);
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr_reverse[now.v]) {
-                if(dist2[t.v] > t.weight + dist2[now.v]) {
-                    dist2[t.v] = t.weight + dist2[now.v];
-                    q.add(new info(t.v, dist2[t.v]));
-                }
-            }
-        }
-        int max = 0;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, dist[i] + dist2[i]);
-        }
-        System.out.println(max);
+        Arrays.sort(card);
+
+        store = new int[m];
+
+        dfs(0);
+        System.out.print(sb);
+
     }
+    
+     public static void dfs(int stage)
+    {
+        if(stage == m)
+        {
+            StringBuilder sb2 = new StringBuilder();
+            for(int i = 0; i < m; i++)
+            {
+                sb2.append(store[i] + " ");
+            }
 
-    static class info implements Comparable<info>{
-        int v, weight;
-        info(int v, int weight){
-            this.v = v;
-            this.weight = weight;
+            if(!set.contains(sb2.toString()))
+            {
+                sb.append(sb2+"\n");
+                set.add(sb2.toString());
+            }
+
+            return;
+
         }
-        @Override
-        public int compareTo(info o) {
-            return weight - o.weight;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(stage > 0 && store[stage-1] > card[i]) continue;
+            store[stage] = card[i];
+            dfs15666(stage+1);
         }
+
     }
 }

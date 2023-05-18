ee1869f304eee38f2edec9b224eeabbbbb0cedbb
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-05-18 16:10:12 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -1,77 +1,71 @@
 import java.io.*;
 import java.util.*;
-
+ 
 public class Main {
-    static int N, M, X;
-    static ArrayList<info>[] arr;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        ArrayList<info>[] arr_reverse = new ArrayList[N+1];
-        arr = new ArrayList[N+1];
-        for(int i=1; i<=N; i++) {
-            arr[i] = new ArrayList<>();
-            arr_reverse[i] = new ArrayList<>();
+ 
+    static int N,K;
+    static int arr[];
+ 
+    public static void main(String args[]) throws Exception {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        N = sc.nextInt(); // 5
+        K = sc.nextInt(); // 17
+        arr = new int[100002];
+ 
+        Arrays.fill(arr, 100000);
+ 
+        if (N == K) {
+            System.out.println("0");
+            return;
         }
-        for(int i=1; i<=M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x =	 Integer.parseInt(st.nextToken());
-            int y =	 Integer.parseInt(st.nextToken());
-            int m =  Integer.parseInt(st.nextToken());
-            arr[x].add(new info(y, m));
-            arr_reverse[y].add(new info(x, m));
+        if (K < N) {
+            System.out.println(N - K);
+            return;
         }
-        int[] dist = new int[N+1];
-        int[] dist2 = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        Arrays.fill(dist2, Integer.MAX_VALUE);
-        dist[X] = 0; dist2[X] = 0;
-        boolean[] visited = new boolean[N+1];
-        PriorityQueue<info> q = new PriorityQueue<>();
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr[now.v]) {
-                if(dist[t.v] > t.weight + dist[now.v]) {
-                    dist[t.v] = t.weight + dist[now.v];
-                    q.add(new info(t.v, dist[t.v]));
-                }
+ 
+        Queue<List> q = new LinkedList();
+ 
+        q.add(Arrays.asList(N,0)); // 5, 0 (현재위치, 소요시간)
+ 
+        int ans = Integer.MAX_VALUE;
+ 
+        while(!q.isEmpty()){
+            List out = q.poll(); // 5 , 0
+ 
+            int cur = (int) out.get(0); // 5
+            int time = (int) out.get(1); // 0
+ 
+            if (time > K) break;
+ 
+            arr[cur] = Math.min(arr[cur], time);
+ 
+            // 2를 곱했을 때, 1을 더했을 때, 1을 뺐을 때를 각각 q에 넣어준다
+ 
+            List nextP = Arrays.asList(cur+1, time+1);
+            List nextN = Arrays.asList(cur-1, time+1);
+            List next2X = Arrays.asList(cur*2, time);
+ 
+//            System.out.println("cur : " + cur + " | time : " + time + " | 다음+1 이동위치 : " + nextP.get(0) + " | 다음-1 이동위치 : " + nextN.get(0) + " | 2배 순간이동 위치 : " + next2X.get(0) + " | ans : " + ans);
+ 
+            if ((int)nextP.get(0) == K) {
+                ans = Math.min(ans, (int)nextP.get(1));
             }
-        }
-        Arrays.fill(visited, false);
-        q.add(new info(X, 0));
-        while (!q.isEmpty()) {
-            info now = q.poll();
-            if(visited[now.v]) continue;
-            visited[now.v] = true;
-            for(info t : arr_reverse[now.v]) {
-                if(dist2[t.v] > t.weight + dist2[now.v]) {
-                    dist2[t.v] = t.weight + dist2[now.v];
-                    q.add(new info(t.v, dist2[t.v]));
-                }
+            if ((int)nextN.get(0) == K) {
+                ans = Math.min(ans, (int)nextN.get(1));
             }
+            if ((int)next2X.get(0) == K) {
+                ans = Math.min(ans, (int)next2X.get(1));
+            }
+ 
+            if ((int)nextP.get(0)<=K+1 && (int)nextP.get(1) < arr[(int)nextP.get(0)]) q.add(nextP); // 다음에 +1로 이동할 좌표가 K+1 범위이내에 있고, 다음에 이동할 좌표의 time값이 이미 arr 배열에 들어있는 time값보다 작을 때만 큐에 넣어준다.
+            if ((int)nextN.get(0)>=0 && (int)nextN.get(1) < arr[(int)nextN.get(0)]) q.add(nextN);
+            if ((int)next2X.get(0)<=K+1 && (int)next2X.get(1) < arr[(int)next2X.get(0)]) q.add(next2X);
         }
-        int max = 0;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(max, dist[i] + dist2[i]);
-        }
-        System.out.println(max);
-    }
-
-    static class info implements Comparable<info>{
-        int v, weight;
-        info(int v, int weight){
-            this.v = v;
-            this.weight = weight;
-        }
-        @Override
-        public int compareTo(info o) {
-            return weight - o.weight;
-        }
+ 
+        System.out.println(ans);
+ 
     }
 }
+ 

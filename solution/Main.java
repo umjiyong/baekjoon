c0876035212caad6eadce2ad1bbdd40075a23708
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-05-23 21:44:52 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -2,12 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, X;
-
-    public static void main(String[] args) throws IOException {
+    static final int P = 1000000007;	
+    public static void main(String[] args) throws IOException{
+      
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        long N = 1, S = 0;
+    
+        for(int i=0;i<M;i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            int n = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+        
+            S = s * N + S * n;
+            N *= n;
+            S %= P;
+            N %= P;
 
-
+        }
+        if(S % N != 0)
+            bw.write((search(N, P-2) * S) % P + "");
+        else	
+            bw.write(S/N + "");
+        bw.flush();		
+        bw.close();
+        br.close();
+    }
+    static long search(long N, int index) {
+        if(index == 1)
+            return N;
+        long temp = search(N, index/2);
+        if(index % 2 == 1)
+            return temp * temp % P * N % P;
+        else
+            return temp * temp % P;
     }
 }

3386a5bd8b9ade94f31d8d354861b93c3add979e
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-05-30 18:12:16 +0900

    Update and rename Main.java to [G4] BJ_1138.java

---
+++ solution/[G4] BJ_1138.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		System.out.println(M - GCD(N, M));
+	}
+    
+    	public static int GCD(int a, int b) {
+		if(b == 0)	return a;
+		return GCD(b, a%b);
+	}
+}

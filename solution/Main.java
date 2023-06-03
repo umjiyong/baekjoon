5e428ccb25d6e22d7c616920d9140c7968aa9142
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-06-03 21:30:03 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -1,13 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M, X;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        char[] board = br.readLine().toCharArray();
 
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < board.length; i++) {
+            if(i + 3 < board.length) {
+                boolean four = board[i] == board[i + 1] && board[i] == board[i + 2] && board[i] == board[i + 3];
+
+                if(four && board[i] == 'X') {
+                    i += 3;
+                    sb.append("AAAA");
+                    continue;
+                }
+            }
+
+            if(i + 1 < board.length) {
+                boolean two = board[i] == board[i + 1];
+
+                if(two && board[i] == 'X') {
+                    i += 1;
+                    sb.append("BB");
+                    continue;
+                }
+            }
+
+            if(board[i] == '.') {
+                sb.append(".");
+            }else {
+                sb = new StringBuilder("-1");
+                break;
+            }
+        }
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
     }
 }

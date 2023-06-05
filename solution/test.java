9db27d96cc4ecf3491e376e027af14ce5fc4e9e5
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-06-05 20:17:33 +0900

    Update test.java

--- solution/test.java
+++ solution/test.java
@@ -1,1 +1,27 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+    
+    public static Integer N;
+    
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.valueOf(br.readLine());
+	    Integer rope[] = new Integer [N];
+        
+		for(int i=0; i<N; i++) {
+			rope[i] = Integer.valueOf(br.readLine());
+		}
+        
+		Arrays.sort(rope, Comparator.reverseOrder());
+        
+		int max = 0;
+        
+		for(int i=0; i<N; i++) {
+			max = Math.max(rope[i]*(i+1), max);
+		}
+        
+		System.out.println(max);
+	}
+}

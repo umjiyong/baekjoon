46746c070ac731fbe4f3500e2453ea1f781f9b3d
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-06-09 21:19:13 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -2,12 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, X;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-
+        
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       String[] s = br.readLine().split("");
+       
+       if(Arrays.asList(s).contains("0")){
+           int[] arr = new int[s.length];
+    	   int sum = 0;
+           for(int i = 0; i < arr.length; i++) {
+        	   arr[i] = Integer.parseInt(s[i]);
+        	   sum += arr[i];
+           }
+           Arrays.sort(arr);
+           if(sum % 3 == 0)
+        	   for(int i = arr.length-1; i > -1; i--) {
+        		   System.out.print(arr[i]);
+           }
+           else
+        	   System.out.println(-1);        	   
+       }
+       else
+    	   System.out.println(-1);
     }
 }

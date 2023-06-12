79c0c9999028a3ffc5137bf08464d3c69ad0d434
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-06-12 21:50:05 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -1,13 +1,16 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, X;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-
-    }
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in);
+		int e = sc.nextInt(), s = sc.nextInt(), m = sc.nextInt();
+		
+		int year = 0;
+		while(true) {
+			year++;
+			if((year-e)%15 == 0 && (year-s)%28 == 0 && (year-m)%19 == 0) break;
+		}
+		System.out.print(year);
+	}
 }

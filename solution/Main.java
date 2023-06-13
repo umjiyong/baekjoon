8d69771c82c6d6b35a6a838181d447297d942f25
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-06-13 21:20:40 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -1,13 +1,37 @@
-import java.io.*;
 import java.util.*;
-
+ 
 public class Main {
-    static int N, M, X;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-
-    }
+	
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		long[] dist = new long[N - 1];	
+		long[] cost = new long[N];	
+		
+		for(int i = 0; i < N - 1; i++) {
+			dist[i] = sc.nextLong();
+		}
+		
+		for(int i = 0; i < N; i++) {
+			cost[i] = sc.nextLong();
+		}
+		
+		long sum = 0;
+		long minCost = cost[0];	
+ 
+		for(int i = 0; i < N - 1; i++) {
+			
+			if(cost[i] < minCost) {
+				minCost = cost[i];
+			}
+			
+			sum += (minCost * dist[i]);	
+		}
+		
+		System.out.println(sum);
+		
+	}
 }

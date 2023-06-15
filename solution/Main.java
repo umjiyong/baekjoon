c85301371280b6f0ef02a32683659edd916b114a
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-06-15 19:04:17 +0900

    Add files via upload

---
+++ solution/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, X;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}

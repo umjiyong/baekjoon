16aa18d31df0fc9addfd516a13864950ff5dd396
umjiyong <98037218+umjiyong@users.noreply.github.com>
2023-06-15 19:04:31 +0900

    Update Main.java

--- solution/Main.java
+++ solution/Main.java
@@ -1,13 +1,26 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, M, X;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String[] s = br.readLine().split("");
+    String[] answer = new String[s.length];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-
+    for(int i = 0; i < s.length; i++) {
+      StringBuilder sb = new StringBuilder();
+      for(int j = i; j < s.length; j++) {
+        sb.append(s[j]);
+      }
+      answer[i] = sb.toString();
     }
+
+    Arrays.sort(answer);
+
+    for(String str : answer) {
+      bw.write(str + "\n");
+    }
+    bw.flush();
+  }
 }
